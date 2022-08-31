f17578d592e18222ee0504b8612b5e8acd1bdcd6
tchouane <antoine.tran.69@gmail.com>
2022-08-31 16:16:32 +0200

    First push of Vue Website

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script>
+  import SlideShow from './components/SlideShow.vue'
+  import PageBand from './components/PageBand.vue'
+  import PageTour from './components/PageTour.vue'
+  import PageContact from './components/PageContact.vue'
+
+  export default{
+    data(){
+      return{
+        page: "band"
+      }
+    },
+    components:{
+    SlideShow,
+    PageBand,
+    PageTour,
+    PageContact
+}
+  }
+</script>
+
+<template>
+ <!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#" @click="page='band'" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+    <a href="#band" @click="page='band'" class="w3-bar-item w3-button w3-padding-large w3-hide-small">BAND</a>
+    <a href="#tour" @click="page='tour'" class="w3-bar-item w3-button w3-padding-large w3-hide-small">TOUR</a>
+    <a href="#contact" @click="page='contact'" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CONTACT</a>
+    <div class="w3-dropdown-hover w3-hide-small">
+      <button class="w3-padding-large w3-button" title="More">MORE <i class="fa fa-caret-down"></i></button>     
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="#" class="w3-bar-item w3-button">Merchandise</a>
+        <a href="#" class="w3-bar-item w3-button">Extras</a>
+        <a href="#" class="w3-bar-item w3-button">Media</a>
+      </div>
+    </div>
+    <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+  </div>
+</div>
+
+<!-- Navbar on small screens (remove the onclick attribute if you want the navbar to always show on top of the content when clicking on the links) -->
+<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
+  <a href="#band" @click="page='band'" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">BAND</a>
+  <a href="#tour" @click="page='tour'" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">TOUR</a>
+  <a href="#contact" @click="page='contact'" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">CONTACT</a>
+  <a href="#" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">MERCH</a>
+</div>
+<!-- End Navbar -->
+<!-- Page content -->
+<div class="w3-content" style="max-width:2000px;margin-top:46px">
+  <SlideShow />
+  <PageBand v-if="page=='band'" />
+  <PageTour v-if="page=='tour'" />
+  <PageContact v-if="page=='contact'" />
+</div>
+<!-- End Page content -->
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+</template>
